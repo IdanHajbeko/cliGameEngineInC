3dc3d0c30b1f0a069cde086906c53c719c960e75
IdanHajbeko <idancode.hajbeko@gmail.com>
2025-05-17 02:09:19 +0300

    the blocks now fall in tetris

--- tests/tetris/main.c
+++ tests/tetris/main.c
@@ -5,121 +5,114 @@
 #include <math.h>
 #include <time.h>
 
-#define MAX(x, y) (((x) > (y)) ? (x) : (y))
-#define MIN(x, y) (((x) < (y)) ? (x) : (y))
-
 struct block{
     short direction;
     struct object obj;
 };
-
-void init_templates(struct object *block1, struct object *block2, struct object *block3,
-                 struct object *block4, struct object *block5, struct object *block6,
-                 struct object *block7, char *backgroundColor);
+struct object block1, block2, block3, block4, block5, block6, block7;
+void init_templates(char *backgroundColor);
 void init_on_screen_block(struct object * template, struct block * blc);
 void init_screen_base_ui();
+struct object * get_random_template();
 
-
-void init_templates(struct object *block1, struct object *block2, struct object *block3,
-                 struct object *block4, struct object *block5, struct object *block6,
-                 struct object *block7, char *backgroundColor) {
-    init_object(block1, 0, 0, 16, 2);
+void init_templates(char *backgroundColor) {
+    init_object(&block1, 0, 0, 16, 2);
     for (int i = 0; i < 2; i++)
         for (int j = 0; j < 16; j++) {
-            block1->space[i][j].ch = ' ';
-            block1->space[i][j].id = 1;
+            block1.space[i][j].ch = ' ';
+            block1.space[i][j].id = 1;
         }
 
-    init_object(block2, 0, 0, 12, 4);
+    init_object(&block2, 0, 0, 12, 4);
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 12; j++) {
-            block2->space[i][j].ch = ' ';
-            block2->space[i][j].id = 0;
-            block2->space[i][j].style=strdup(backgroundColor);
+            block2.space[i][j].ch = ' ';
+            block2.space[i][j].id = 0;
+            block2.space[i][j].style=strdup(backgroundColor);
         }
     for (int i = 0; i < 4; i++) {
         if (i < 2) {
             for (int j = 0; j < 4; j++) {
-                block2->space[i][j].id = 1;
+                block2.space[i][j].id = 1;
             }
         } else {
             for (int j = 0; j < 12; j++) {
-                block2->space[i][j].id = 1;
+                block2.space[i][j].id = 1;
             }
         }
     }
 
-    init_object(block3, 0, 0, 12, 4);
+    init_object(&block3, 0, 0, 12, 4);
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 12; j++) {
-            block3->space[i][j].ch = ' ';
-            block3->space[i][j].id = 0;
-            block3->space[i][j].style=strdup(backgroundColor);
+            block3.space[i][j].ch = ' ';
+            block3.space[i][j].id = 0;
+            block3.space[i][j].style=strdup(backgroundColor);
         }
     for (int i = 0; i < 4; i++) {
         if (i < 2) {
             for (int j = 7; j < 12; j++) {
-                block3->space[i][j].id = 1;
+                block3.space[i][j].id = 1;
             }
         } else {
             for (int j = 0; j < 12; j++) {
-                block3->space[i][j].id = 1;
+                block3.space[i][j].id = 1;
             }
         }
     }
 
-    init_object(block4, 0, 0, 8, 4);
+    init_object(&block4, 0, 0, 8, 4);
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 8; j++) {
-            block4->space[i][j].ch = ' ';
-            block4->space[i][j].id = 1;
+            block4.space[i][j].ch = ' ';
+            block4.space[i][j].id = 1;
         }
 
-    init_object(block5, 0, 0, 12, 4);
+    init_object(&block5, 0, 0, 12, 4);
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 12; j++) {
-            block5->space[i][j].ch = ' ';
-            block5->space[i][j].id = 0;
-            block5->space[i][j].style=strdup(backgroundColor);
+            block5.space[i][j].ch = ' ';
+            block5.space[i][j].id = 0;
+            block5.space[i][j].style=strdup(backgroundColor);
         }
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 12; j++) {
             if ((i < 2 && j > 3) || (i >= 2 && j < 8)) {
-                block5->space[i][j].id = 1;
+                block5.space[i][j].id = 1;
             }
         }
     }
 
-    init_object(block6, 0, 0, 12, 4);
+    init_object(&block6, 0, 0, 12, 4);
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 12; j++) {
-            block6->space[i][j].ch = ' ';
-            block6->space[i][j].id = 0;
-            block6->space[i][j].style=strdup(backgroundColor);
+            block6.space[i][j].ch = ' ';
+            block6.space[i][j].id = 0;
+            block6.space[i][j].style=strdup(backgroundColor);
         }
     for (int i = 0; i < 4; i++) {
         if (i < 2) {
             for (int j = 4; j < 8; j++) {
-                block6->space[i][j].id = 1;
+                block6.space[i][j].id = 1;
             }
         } else {
             for (int j = 0; j < 12; j++) {
-                block6->space[i][j].id = 1;
+                block6.space[i][j].id = 1;
             }
         }
     }
 
-    init_object(block7, 0, 0, 12, 4);
+    init_object(&block7, 0, 0, 12, 4);
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 12; j++) {
-            block7->space[i][j].ch = ' ';
-            block7->space[i][j].id = 0;
-            block7->space[i][j].style=strdup(backgroundColor);
+            block7.space[i][j].ch = ' ';
+            block7.space[i][j].id = 0;
+            block7.space[i][j].style=strdup(backgroundColor);
         }
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 12; j++) {
             if ((i < 2 && j < 8) || (i >= 2 && j > 3)) {
-                block7->space[i][j].id = 1;
+                block7.space[i][j].id = 1;
             }
         }
     }
@@ -170,7 +163,17 @@ void init_screen_base_ui(){
     }
 }
 
-
+struct object * get_random_template(){
+    switch(rand() %7 +1){
+        case 1: return &block1; break;
+        case 2: return &block2; break;
+        case 3: return &block3; break;
+        case 4: return &block4; break;
+        case 5: return &block5; break;
+        case 6: return &block6; break;
+        case 7: return &block7; break;
+    }
+}
 
 int main() {
     srand(time(0));
@@ -178,27 +181,35 @@ int main() {
     init_screen(' ', "\e[47m");
     startKeyListener();
 
-    struct object block1, block2, block3, block4, block5, block6, block7;
-    init_templates(&block1, &block2, &block3, &block4, &block5, &block6, &block7, "\e[40    m");
+    init_templates("\e[40m");
 
-    struct block onBlock1, onBlock2;
-    init_on_screen_block(&block5, &onBlock1);
-    init_on_screen_block(&block1, &onBlock2);
+
+    int time = 0;
+    struct block *currentBlock = malloc(sizeof(struct block));
+    if (!currentBlock) {
+        perror("Failed to allocate memory");
+        exit(1);
+    }
     init_screen_base_ui();
-    put_object(&onBlock1.obj);
-
+    init_on_screen_block(get_random_template(), currentBlock);
+    put_object(&currentBlock->obj);
     while(1){
         int key = last_key;
         last_key = 999;
         if (key == KEY_ESC || key == 'q') break;
 
-
-
+        if (time >= 500){
+            if (currentBlock->obj.y2 != screenSizeY/2+20) move_object(&currentBlock->obj, currentBlock->obj.x, currentBlock->obj.y+1, ' ', "\e[40m");
+            else {
+                init_on_screen_block(get_random_template(), currentBlock);
+                put_object(&currentBlock->obj);
+            }
+            time = 0;
+        }
         print_screen();
         sleep_ms(16);
+        time += 16;
     }
-
-
 
     printf("\e[?25h");
     fflush(stdout);
@@ -210,8 +221,7 @@ int main() {
     free_object(&block5);
     free_object(&block6);
     free_object(&block7);
-    free_object(&onBlock1.obj);
-    free_object(&onBlock2.obj);
+    free_object(&currentBlock->obj);
 
     free_screen();
     return 0;
